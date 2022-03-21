0d6642102a5abce287c729a10bf61e582c941231
Samanyu <35706393+Samanyu24X@users.noreply.github.com>
2022-03-20 23:22:17 -0700

    Added required features worked on in Lab 3 in Week 5

---
+++ app/src/main/java/com/example/samanyusatheeshflashcardapp/AppDatabase.java
@@ -0,0 +1,9 @@
+package com.example.samanyusatheeshflashcardapp;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Flashcard.class}, version = 3)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract FlashcardDao flashcardDao();
+}
--- app/src/main/java/com/example/samanyusatheeshflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/samanyusatheeshflashcardapp/MainActivity.java
@@ -12,16 +12,30 @@ import com.google.android.material.snackbar.Snackbar;
 
 import org.w3c.dom.Text;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashCards;
     TextView flashcardQuestion;
     TextView flashcardAnswer;
     TextView answerChoiceOne, answerChoiceTwo, answerChoiceThree;
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flashcardDatabase = new FlashcardDatabase(this);
+        allFlashCards = flashcardDatabase.getAllCards();
+
+        if (allFlashCards != null && allFlashCards.size() > 0)
+        {
+            flashcardQuestion.setText(allFlashCards.get(0).getQuestion());
+            flashcardAnswer.setText(allFlashCards.get(0).getAnswer());
+        }
 
         flashcardQuestion = findViewById(R.id.flashcard_question_textview);
         flashcardAnswer = findViewById(R.id.flashcard_answer_textview);
@@ -30,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         answerChoiceThree = findViewById(R.id.answer_option_three);
         ImageView flashcardAdd = findViewById(R.id.flashcard_add_button);
         ImageView flashcardEdit = findViewById(R.id.flashcard_edit_button);
+        ImageView flashcardNext = findViewById(R.id.flashcard_next_button);
 
         flashcardQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,6 +104,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        flashcardNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (allFlashCards.size() == 0)
+                    return;
+                currentCardDisplayedIndex++;
+
+                if (currentCardDisplayedIndex >= allFlashCards.size())
+                {
+                    Snackbar.make(flashcardQuestion,"Reached the last card, going back to start.",
+                        Snackbar.LENGTH_SHORT).show();
+                    currentCardDisplayedIndex = 0;
+                }
+
+                allFlashCards = flashcardDatabase.getAllCards();
+                Flashcard flashcard = allFlashCards.get(currentCardDisplayedIndex);
+                flashcardQuestion.setText(flashcard.getQuestion());
+                flashcardAnswer.setText(flashcard.getAnswer());
+            }
+        });
+
     }
 
     @Override
@@ -99,8 +135,7 @@ public class MainActivity extends AppCompatActivity {
         answerChoiceTwo.setBackgroundColor(getResources().getColor(R.color.my_tan, null));
         answerChoiceThree.setBackgroundColor(getResources().getColor(R.color.my_tan, null));
 
-
-        if (requestCode == 100)
+        /* if (requestCode == 100)
         {
             if (data != null) {
                 String questionString = data.getExtras().getString("QUESTION_KEY");
@@ -114,10 +149,21 @@ public class MainActivity extends AppCompatActivity {
                 answerChoiceThree.setText(rightAnswerString);
 
             }
+        } */
+        if (requestCode == 100 && resultCode == RESULT_OK)
+        {
+            String question = data.getExtras().get("QUESTION_KEY").toString();
+            String answer = data.getExtras().get("RIGHTANSWER_KEY").toString();
+            flashcardQuestion.setText(question);
+            flashcardAnswer.setText(answer);
+
+            flashcardDatabase.insertCard(new Flashcard(question, answer));
+            allFlashCards = flashcardDatabase.getAllCards();
+
+            Snackbar.make(flashcardQuestion,
+                    "Successfully saved card!",
+                    Snackbar.LENGTH_SHORT)
+                    .show();
         }
-        Snackbar.make(flashcardQuestion,
-                "Successfully saved card!",
-                Snackbar.LENGTH_SHORT)
-                .show();
     }
 }
